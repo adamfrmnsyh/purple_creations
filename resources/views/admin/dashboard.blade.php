da2e3cb9f1644919538ea3cfdd8dcd704e7a9420
adamfrmnsyh <xilentz507@gmail.com>
2025-10-25 00:39:43 +0700

    menambahkan dashboard admin menggunakan laravel breeze

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="p-6 text-gray-900">
+        <h1 class="text-2xl font-bold">Selamat datang, Admin!</h1>
+        <p>Ini adalah halaman dashboard admin.</p>
+    </div>
+</x-app-layout>
