139c155341c0337299c001ca02b9ad238092af43
adamfrmnsyh <xilentz507@gmail.com>
2026-01-13 21:58:44 +0700

    ganti aboutUs

--- resources/views/Home/aboutUs.blade.php
+++ resources/views/Home/aboutUs.blade.php
@@ -3,7 +3,7 @@
     @section('title', 'About Us | Purple Creations')
 
     <x-navbar />
-    <x-hero-aboutus />
+    <x-hero-aboutUs />
 
     <div class="bg-[#fdeef7]">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
